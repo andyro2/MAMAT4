45fe0fa619f162089b858cfe6dc748fb98512f04
Andy-Adrian Rodan <a.rodan@campus.technion.ac.il>
2018-06-02 20:09:46 +0300

    Class start

--- MAMAT4/Class.C
+++ MAMAT4/Class.C
@@ -5,4 +5,26 @@
 using std::cout;
 using std::endl;
 
-// Add your code here ...
+Class::Class(int const room_num, int size, double max_ratio, int ages, int max_capacitence, bool occupied)
+	:Room(room_num, size, occupied), max_ratio_(max_ratio), ages_(ages), max_capacitence_(max_capacitence) {};
+
+int Class::Get_Num_Teachers() const {
+	return teachers_.size();
+}
+
+int Class::Get_Ages() const {
+	return ages_;
+}
+
+double Class::Get_Ratio() const {
+	return max_ratio_;
+}
+
+string Class::Get_Child_Phone(string const child_name) const {
+	for (int i = 0 ; i<children_.size(); ++i) {
+		if (child_name == children_[i].GetName())
+			return children_[i].GetPhone();
+	}
+
+	return "No Child";
+}
